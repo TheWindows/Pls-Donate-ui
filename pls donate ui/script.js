9d41e91cec5ba8072d007dbe6aa1e1ad502e174e
TheWindows <151549510+TheWindows@users.noreply.github.com>
2024-12-05 14:41:57 +0330

    Add files via upload

---
+++ pls donate ui/script.js
@@ -0,0 +1,49 @@
+document.getElementById('shop').addEventListener('click', function() {
+    const toggleDiv = document.getElementById('toggleDiv');
+    const upper = document.getElementById('upper');
+    toggleDiv.classList.toggle('hidden');
+    upper.classList.toggle('hidden');
+});
+document.getElementById('shop-close').addEventListener('click', function() {
+    const toggleDiv = document.getElementById('toggleDiv');
+    const upper = document.getElementById('upper');
+    toggleDiv.classList.toggle('hidden');
+    upper.classList.toggle('hidden');
+}); 
+document.getElementById('redeem').addEventListener('click', function() {
+    const toggleDiv = document.getElementById('toggleDiv2');
+    const upper = document.getElementById('upper2');
+    toggleDiv.classList.toggle('hidden2');
+    upper.classList.toggle('hidden2');
+}); 
+document.getElementById('redeem-close').addEventListener('click', function() {
+    const toggleDiv = document.getElementById('toggleDiv2');
+    const upper = document.getElementById('upper2');
+    toggleDiv.classList.toggle('hidden2');
+    upper.classList.toggle('hidden2');
+}); 
+document.getElementById('setting').addEventListener('click', function() {
+    const toggleDiv = document.getElementById('toggleDiv3');
+    const upper = document.getElementById('upper3');
+    toggleDiv.classList.toggle('hidden3');
+    upper.classList.toggle('hidden3');
+}); 
+document.getElementById('setting-close').addEventListener('click', function() {
+    const toggleDiv = document.getElementById('toggleDiv3');
+    const upper = document.getElementById('upper3');
+    toggleDiv.classList.toggle('hidden3');
+    upper.classList.toggle('hidden3');
+}); 
+document.getElementById('gift').addEventListener('click', function() {
+    const toggleDiv2 = document.getElementById('toggleDiv4');
+    const upper2 = document.getElementById('upper4');
+    toggleDiv2.classList.toggle('hidden4');
+    upper2.classList.toggle('hidden4');
+}); 
+document.getElementById('gift-close').addEventListener('click', function() {
+    const toggleDiv = document.getElementById('toggleDiv4');
+    const upper = document.getElementById('upper4');
+    toggleDiv.classList.toggle('hidden4');
+    upper.classList.toggle('hidden4');
+}); 
+// im bad at js dont blame me :)
